24e52a3b24b5f21b5fb5bfc66974bc92c592e2e4
Andy Amaya <aamay001@gmail.com>
2018-01-23 13:14:03 -0800

    resources

---
+++ resources.js
@@ -0,0 +1,25 @@
+/*
+
+History of JavaScript
+https://auth0.com/blog/a-brief-history-of-javascript/
+
+Equality Checking
+http://dorey.github.io/JavaScript-Equality-Table/
+
+W3 Schools
+https://www.w3schools.com/js/default.asp
+
+Mozilla Developer Network
+https://developer.mozilla.org/en-US/docs/Web/JavaScript
+
+Stack Overflow
+https://stackoverflow.com/
+
+The Definitive JavaScript Handbook
+https://medium.freecodecamp.org/the-definitive-javascript-handbook-for-a-developer-interview-44ffc6aeb54e
+
+Instructional Videos
+https://www.youtube.com/channel/UCO1cgjhGzsSYb1rsB4bFe4Q // Fun Fun Function
+https://www.youtube.com/channel/UC2JNTaq_crzfZOu9Aq96LsA // JavaScriptLA
+
+*/
